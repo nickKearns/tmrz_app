a0a49e0c0cc6c646da2fc2a2a523f81f019b3969
nickKearns <nickkearns133@gmail.com>
2021-12-02 21:10:05 -0500

    added new timer input

---
+++ src/components/NewTimer.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { addTimer } from '../actions'
+
+export default function NewTimer() {
+    const [ name, setName ] = useState('')
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+            <input type='text'
+            placeholder='New Timer name'
+            name='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}/>
+            <button
+            onClick={(e) => dispatch(addTimer(name))}>
+                Save
+            </button>
+
+        </div>
+    )
+}
